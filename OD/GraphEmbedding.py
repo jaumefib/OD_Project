5eb245cb772770f78a60f2c2936fc58da9c4ea82
Atomorojo <tonicasas6@gmail.com>
2021-06-22 21:45:22 +0200

    Minor graphic changes

--- OD/GraphEmbedding.py
+++ OD/GraphEmbedding.py
@@ -33,13 +33,16 @@ class GraphEmbedding(object):
                 else:
                     listAux = [target]
                     self.nodeAdjacency[origin] = listAux
-                #if target in self.nodeAdjacency:
-                    #    listAux = self.nodeAdjacency[target]
-                    #    listAux.append(origin)
-                #    self.nodeAdjacency[target] = listAux
-                #else:
-                    #    listAux = [origin]
-                #    self.nodeAdjacency[target] = listAux
+
+        for origin in self.nodes:
+            if origin in self.nodeAdjacency:
+                listAux = self.nodeAdjacency[origin]
+                listAux.append(origin)
+                self.nodeAdjacency[origin] = listAux
+            else:
+                listAux = [origin]
+                self.nodeAdjacency[origin] = listAux
+
 
         with open('musae_facebook_target.csv', newline='',encoding="utf8") as csvfile:
             reader = csv.reader(csvfile, delimiter='\n', quotechar='"')
@@ -52,18 +55,17 @@ class GraphEmbedding(object):
         toFactorize = np.zeros(shape=(len(self.nodes), len(self.nodes)))
 
         for key in sorted(self.nodeAdjacency):
-            toFactorize[key][key]=1
             for val in self.nodeAdjacency[key]:
                 toFactorize[key][val] = 1
 
-        svd = TruncatedSVD(n_components=500, n_iter=1, random_state=42)
+        svd = TruncatedSVD(n_components=500, n_iter=10, random_state=42)
         svd.fit(toFactorize)
         self.result = svd.transform(toFactorize)
 
     def doWeHaveIt(self, page_name):
         for i in self.nodeLabel:
             if page_name in self.nodeLabel[i]:
-                print("Did you ment the page " + self.nodeLabel[i] + " ?")
+                print("Did you mean the page " + self.nodeLabel[i] + " ?")
                 correct_answer = False
                 while not correct_answer:
                     is_the_one = str(input("'y' for yes, 'n' for no: "))
@@ -102,7 +104,7 @@ class GraphEmbedding(object):
 
         i = 0
         for w in sorted(results, key=results.get, reverse=False):
-            print("    " + str(i) + ". " + self.nodeLabel[w] + " - " + str(int(results[w])))
+            print("    " + str(i) + ". " + self.nodeLabel[w] + " - " + str(np.linalg.norm(self.result[w]-self.result[index])))
             i = i+1
             if i == 20:
                 break
